60fa0ad8cbb782b131a9f6b30285ba0b1055aea2
Miriam <miriamsd94@gmail.com>
2026-07-06 23:38:53 +0200

    modified application and user model

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    //
+    protected $fillable = ['user_id', 'offer_id', 'message', 'status'];
+
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    
+    public function offer()
+    {
+        return $this->belongsTo(Offer::class);
+    }
 }
